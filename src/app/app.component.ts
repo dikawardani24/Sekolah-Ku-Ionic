686d226bd612307cf90636a0e19fbd0b501474c3
Dika Wardani <dikawardani24@gmail.com>
2018-06-21 06:32:32 +0700

    FInishing support search siswa by name module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,7 +26,8 @@ export class MyApp {
 
           // Okay, so the platform is ready and our plugins are available.
           // Here you can do any higher level native things you might need.
-          statusBar.styleDefault();
+          statusBar.styleBlackTranslucent();
+
           console.log("Closing the splash")
           splashScreen.hide();
         }
